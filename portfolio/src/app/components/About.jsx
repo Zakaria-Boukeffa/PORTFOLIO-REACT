9427faec9a1eefa1094926c8011549a1bcdcebeb
Zakaria Boukeffa <zakaria.boukeffa.01@gmail.com>
2025-06-07 14:09:02 +0200

    About section fixed again, Do not re-touch it okey ?

--- portfolio/src/app/components/About.jsx
+++ portfolio/src/app/components/About.jsx
@@ -10,10 +10,10 @@ function About() {
           Hi there, I’m Zakaria BOUKEFFA, a Computer Science student with a deep enthusiasm for Data Science and Artificial Intelligence.
         </p>
         <p>
-          I approach problems with a critical mindset, focusing on breaking down complex issues into manageable parts and finding practical, innovative, and effective solutions.
+          I approach problems with a critical mindset, focusing on breaking down complex issues into manageable parts and finding effective, practical, and innovative solutions.
         </p>
         <p>
-          Currently, I’m focused on data-related topics and deep learning, but I also work on app development from time to time.
+          Currently, I’m focused on deep learning and data-related topics, but I also do app development from time to time.
         </p>
       </div>
     </section>
